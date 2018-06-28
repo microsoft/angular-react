9749d4e232e08274dee5b509ccc2583445c50684
Ben Grynhaus <bengr@microsoft.com>
2018-06-28 16:40:22 +0300

    Amend to c8e174edbbff1ef42bf543ba6bff18e4fac431c2

---
+++ libs/core/src/utils/object/remove-undefined-properties.ts
@@ -0,0 +1,9 @@
+const clearUndefinedProperties = <T extends object>(obj: T): Partial<T> => {
+  return Object.keys(obj).reduce((acc, key) => {
+    const _acc = acc;
+    if (obj[key] !== undefined) _acc[key] = obj[key];
+    return _acc;
+  }, {})
+}
+
+export default clearUndefinedProperties;
