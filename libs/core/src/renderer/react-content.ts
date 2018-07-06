a2dec9525efb8ab13544483416508d601eacb162
Ben Grynhaus <bengr@microsoft.com>
2018-07-06 15:05:23 +0300

    Changed API of ReactContent's render mode

--- libs/core/src/renderer/react-content.ts
+++ libs/core/src/renderer/react-content.ts
@@ -10,28 +10,31 @@ export interface ReactContentProps {
   /**
    * Experimental rendering mode.
    * Uses a similar approach to `router-outlet`, where the child elements are added to the parent, instead of this node, and this is hidden.
-   * @default true
+   * @default false
    */
-  experimentalMode?: boolean;
+  legacyRenderMode?: boolean;
 }
 
+/**
+ * Render any `HTMLElement`s (including Angular components) as a child of React components.
+ * Supports two rendering modes:
+ *  1. `legacy` - append `<react-content>` as the root, and nest the `children-to-append` underneath it.
+ *  2. `new` (**default**) - append the `children-to-append` to the parent of this component, and hide the `<react-content>` element.
+ *     (similar to how `router-outlet` behaves in Angular).
+ */
 export class ReactContent extends React.PureComponent<ReactContentProps> {
-
-  static defaultProps: Partial<ReactContentProps> = {
-    experimentalMode: true,
-  };
 
   componentDidMount() {
     const element = ReactDOM.findDOMNode(this);
     if (this.props[CHILDREN_TO_APPEND_PROP]) {
       if (DEBUG) { console.warn('ReactContent Component > componentDidMount > childrenToAppend:', this.props[CHILDREN_TO_APPEND_PROP]); }
 
-      const hostElement = this.props.experimentalMode ? element.parentElement : element;
+      const hostElement = this.props.legacyRenderMode ? element : element.parentElement;
       this.props[CHILDREN_TO_APPEND_PROP].forEach(child => hostElement.appendChild(child));
     }
   }
 
   render() {
-    return React.createElement('react-content', this.props.experimentalMode && { style: { display: 'none' } });
+    return React.createElement('react-content', !this.props.legacyRenderMode && { style: { display: 'none' } });
   }
 }
