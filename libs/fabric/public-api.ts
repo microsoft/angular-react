78be365fa76c5551ebbce353581ec50e8abfe473
Ben Grynhaus <bengr@microsoft.com>
2018-07-08 18:07:30 +0300

    Remove pickers from public api (lots of warnings at import)

--- libs/fabric/public-api.ts
+++ libs/fabric/public-api.ts
@@ -24,5 +24,5 @@ export * from './src/components/tooltip/public-api';
 export * from './src/components/modal/public-api';
 export * from './src/components/shimmer/public-api';
 export * from './src/components/slider/public-api';
-export * from './src/components/pickers/public-api';
+// export * from './src/components/pickers/public-api';
 export * from './src/components/search-box/public-api';
