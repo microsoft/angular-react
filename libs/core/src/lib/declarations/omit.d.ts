ba64b4f4bbe712de51b675522b86bf14bc7f42f9
Ben Grynhaus <bengr@microsoft.com>
2018-10-15 23:42:42 +0300

    Better Omit type

--- libs/core/src/lib/declarations/omit.d.ts
+++ libs/core/src/lib/declarations/omit.d.ts
@@ -1,4 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+import { IndexSignature } from './index-signature';
+import { KnownKeys } from './known-keys';
+
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<KnownKeys<T> & keyof T, K>> & IndexSignature<T>;
