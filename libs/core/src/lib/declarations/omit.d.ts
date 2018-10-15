e584058b0df97a43e2b075426a2c6ae330988d24
Ben Grynhaus <bengr@microsoft.com>
2018-10-16 00:36:19 +0300

    Remove types that didn't work well

--- libs/core/src/lib/declarations/omit.d.ts
+++ libs/core/src/lib/declarations/omit.d.ts
@@ -1,11 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { IndexSignature } from './index-signature';
 import { KnownKeys } from './known-keys';
-import { UnknownKeys } from './unknown-keys';
 
-export type _KnownKeysOmit<T, K extends keyof T> = Pick<T, Exclude<KnownKeys<T> & keyof T, K>>;
-export type Omit<T, K extends keyof T> = UnknownKeys<T> extends never
-  ? _KnownKeysOmit<T, K>
-  : _KnownKeysOmit<T, K> & IndexSignature<T>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<KnownKeys<T> & keyof T, K>>;
