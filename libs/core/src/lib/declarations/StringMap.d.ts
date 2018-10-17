59ca73a69aca03b29e938b076c7ff4f4deba18be
Ben Grynhaus <bengr@microsoft.com>
2018-10-17 23:22:14 +0300

    Fix ICommandBarItemOptions type (again)

--- libs/core/src/lib/declarations/StringMap.d.ts
+++ libs/core/src/lib/declarations/StringMap.d.ts
@@ -1,4 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+/**
+ * An object with a `string`-based index signature.
+ */
 export type StringMap<T = any> = { [index: string]: T };
