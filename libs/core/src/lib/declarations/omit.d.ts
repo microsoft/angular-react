271ce22e539ef3a979e8965173359a3ffaf57e4b
Ben Grynhaus <bengr@microsoft.com>
2018-10-19 22:49:19 +0300

    Some docs and import optimization

--- libs/core/src/lib/declarations/omit.d.ts
+++ libs/core/src/lib/declarations/omit.d.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { KnownKeys } from './known-keys';
-
+/**
+ * Inverse of `Pick<T, K>`.
+ */
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<KnownKeys<T> & keyof T, K>>;
