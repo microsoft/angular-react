4942b21d7c5b156a4499d174a67709af32f542fa
Ben Grynhaus <bengr6@gmail.com>
2018-12-04 10:13:53 +0200

    Rename EventListener array and map to `I`-prefied versions to avoid confusion

--- libs/core/src/lib/@types/geteventlisteners.d.ts
+++ libs/core/src/lib/@types/geteventlisteners.d.ts
@@ -4,20 +4,18 @@ interface IEventListener<K extends keyof ElementEventMap> {
   options?: boolean | EventListenerOptions;
 }
 
-type EventListenerArray<K extends keyof ElementEventMap> = IEventListener<K>[];
+type IEventListenerArray<K extends keyof ElementEventMap> = IEventListener<K>[];
 
-type EventListenersMap<K extends keyof ElementEventMap> = Record<K, EventListenerArray<K>>;
+type IEventListenersMap<K extends keyof ElementEventMap> = Record<K, IEventListenerArray<K>>;
 
-// declare global {
 interface Element {
   /**
    * Gets all the event listeners of the element.
    */
-  getEventListeners<K extends keyof ElementEventMap>(): EventListenersMap<K>;
+  getEventListeners<K extends keyof ElementEventMap>(): IEventListenersMap<K>;
 
   /**
    * Gets all the event listeners of a type of the element.
    */
-  getEventListeners<K extends keyof ElementEventMap>(type?: K): EventListenerArray<K>;
+  getEventListeners<K extends keyof ElementEventMap>(type?: K): IEventListenerArray<K>;
 }
-// }
