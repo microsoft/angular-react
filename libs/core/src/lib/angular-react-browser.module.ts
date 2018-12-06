c22495926a6afb8dd6f7b77b5926211dfd777cb9
Ben Grynhaus <bengr6@gmail.com>
2018-12-06 17:45:50 +0200

    Moved geteventlisteners to be part of the core package

--- libs/core/src/lib/angular-react-browser.module.ts
+++ libs/core/src/lib/angular-react-browser.module.ts
@@ -3,7 +3,7 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule, ɵDomRendererFactory2 } from '@angular/platform-browser';
-import 'geteventlisteners';
+import './external/geteventlisteners';
 import { AngularReactRendererFactory } from './renderer/renderer';
 
 @NgModule({
