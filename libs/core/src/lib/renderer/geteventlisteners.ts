7f691d79727e77478ff13362c1399c949bb21bd8
Ben Grynhaus <bengr6@gmail.com>
2018-12-11 19:29:12 +0200

    Fix geteventlisteners being loaded twice (#56)
    * Prevent stack overflow for geteventlisteners hook
    
    * Fix geteventlisteners import to work with HMR as well

--- libs/core/src/lib/renderer/geteventlisteners.ts
+++ libs/core/src/lib/renderer/geteventlisteners.ts
@@ -1,77 +1,76 @@
-// Taken and modified from https://github.com/colxi/getEventListeners to be compiled into ES5, allowing running in older browsers
-
 /**
  * Monkey-patches `Element`'s `addEventListener` & `removeEventListener` and adds `getEventListeners`.
  * This later allows the renderer to emit any event handlers attached to React-wrapped components as Angular Outputs:
-```html
-<my-component (arbitraryEvent)="onEventHandler($event)"></my-component>
-```
- */
-export function initHooks() {
-  // save the original methods before overwriting them
-  Element.prototype['_addEventListener'] = Element.prototype.addEventListener;
-  Element.prototype['_removeEventListener'] = Element.prototype.removeEventListener;
+ * ```html
+ * <my-component (arbitraryEvent)="onEventHandler($event)"></my-component>
+ * ```
+ *
+ * @note Taken and modified from https://github.com/colxi/getEventListeners to be compiled into ES5, allowing running in older browsers
+ **/
 
-  Element.prototype.addEventListener = function<K extends keyof ElementEventMap>(
-    type: K,
-    listener: (this: Element, ev: ElementEventMap[K]) => any,
-    options?: boolean | AddEventListenerOptions
-  ): void {
-    if (options === undefined) options = false;
+Element.prototype['_addEventListener'] = Element.prototype.addEventListener;
+Element.prototype['_removeEventListener'] = Element.prototype.removeEventListener;
 
-    // declare listener
-    this._addEventListener(type, listener, options);
+Element.prototype.addEventListener = function<K extends keyof ElementEventMap>(
+  type: K,
+  listener: (this: Element, ev: ElementEventMap[K]) => any,
+  options?: boolean | AddEventListenerOptions
+): void {
+  if (options === undefined) options = false;
 
-    if (!this.eventListenerList) this.eventListenerList = {};
-    if (!this.eventListenerList[type]) this.eventListenerList[type] = [];
+  // declare listener
+  this._addEventListener(type, listener, options);
 
-    // add listener to  event tracking list
-    this.eventListenerList[type].push({
-      type: type,
-      listener: listener,
-      useCapture: options,
-    });
-  };
+  if (!this.eventListenerList) this.eventListenerList = {};
+  if (!this.eventListenerList[type]) this.eventListenerList[type] = [];
 
-  Element.prototype.removeEventListener = function<K extends keyof ElementEventMap>(
-    type: K,
-    listener: (this: Element, ev: ElementEventMap[K]) => any,
-    options?: boolean | EventListenerOptions
-  ): void {
-    if (options === undefined) options = false;
+  // add listener to  event tracking list
+  this.eventListenerList[type].push({
+    type: type,
+    listener: listener,
+    useCapture: options,
+  });
+};
 
-    // remove listener
-    this._removeEventListener(type, listener, options);
+Element.prototype.removeEventListener = function<K extends keyof ElementEventMap>(
+  type: K,
+  listener: (this: Element, ev: ElementEventMap[K]) => any,
+  options?: boolean | EventListenerOptions
+): void {
+  if (options === undefined) options = false;
 
-    if (!this.eventListenerList) this.eventListenerList = {};
-    if (!this.eventListenerList[type]) this.eventListenerList[type] = [];
+  // remove listener
+  this._removeEventListener(type, listener, options);
 
-    // Find the event in the list, If a listener is registered twice, one
-    // with capture and one without, remove each one separately. Removal of
-    // a capturing listener does not affect a non-capturing version of the
-    // same listener, and vice versa.
-    for (let i = 0; i < this.eventListenerList[type].length; i++) {
-      if (
-        this.eventListenerList[type][i].listener === listener &&
-        this.eventListenerList[type][i].useCapture === options
-      ) {
-        this.eventListenerList[type].splice(i, 1);
-        break;
-      }
+  if (!this.eventListenerList) this.eventListenerList = {};
+  if (!this.eventListenerList[type]) this.eventListenerList[type] = [];
+
+  // Find the event in the list, If a listener is registered twice, one
+  // with capture and one without, remove each one separately. Removal of
+  // a capturing listener does not affect a non-capturing version of the
+  // same listener, and vice versa.
+  for (let i = 0; i < this.eventListenerList[type].length; i++) {
+    if (
+      this.eventListenerList[type][i].listener === listener &&
+      this.eventListenerList[type][i].useCapture === options
+    ) {
+      this.eventListenerList[type].splice(i, 1);
+      break;
     }
-    // if no more events of the removed event type are left,remove the group
-    if (this.eventListenerList[type].length == 0) delete this.eventListenerList[type];
-  };
+  }
+  // if no more events of the removed event type are left,remove the group
+  if (this.eventListenerList[type].length == 0) delete this.eventListenerList[type];
+};
 
-  Element.prototype.getEventListeners = function<K extends keyof ElementEventMap>(type?: K) {
-    if (!this.eventListenerList) this.eventListenerList = {};
+Element.prototype.getEventListeners = function<K extends keyof ElementEventMap>(type?: K) {
+  if (!this.eventListenerList) this.eventListenerList = {};
 
-    // return requested listeners type or all them
-    if (type === undefined) return this.eventListenerList;
-    return this.eventListenerList[type];
-  };
+  // return requested listeners type or all them
+  if (type === undefined) return this.eventListenerList;
+  return this.eventListenerList[type];
+};
 
-  /*
+/*
     Element.prototype.clearEventListeners = function(a){
         if(!this.eventListenerList)
             this.eventListenerList = {};
@@ -88,4 +87,3 @@ export function initHooks() {
         }
     };
     */
-}
