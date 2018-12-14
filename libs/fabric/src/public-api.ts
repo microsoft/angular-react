344b56aef0799e970ce574007f98f6aa64a03d9d
Ben Grynhaus <bengr6@gmail.com>
2018-12-14 18:35:54 +0200

    [fabric] add text field (and masked text field) component

--- libs/fabric/src/public-api.ts
+++ libs/fabric/src/public-api.ts
@@ -31,6 +31,7 @@ export * from './lib/components/search-box/public-api';
 export * from './lib/components/shimmer/public-api';
 export * from './lib/components/slider/public-api';
 export * from './lib/components/spinner/public-api';
+export * from './lib/components/text-field/public-api';
 export * from './lib/components/toggle/public-api';
 export * from './lib/components/tooltip/public-api';
 
