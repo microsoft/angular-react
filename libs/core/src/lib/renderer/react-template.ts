9210187960692fa56f55900f9edf5ce74065ff0a
Ben Grynhaus <bengr6@gmail.com>
2019-01-26 20:27:31 +0200

    always get the leading and trailing values, to ensure that we stay up-to-date. See https://github.com/Microsoft/angular-react/issues/79#issuecomment-457853778 for more details

--- libs/core/src/lib/renderer/react-template.ts
+++ libs/core/src/lib/renderer/react-template.ts
@@ -88,9 +88,11 @@ export class ReactTemplate<TContext extends object | void> extends React.Compone
 
     // Throttling the detect changes to an empirically selected value so we don't overload too much work.
     // TODO: This needs some better solution to listen to changes to the binding sources of the template.
-    this._ngZoneSubscription = ngZone.onStable.pipe(throttleTime(TEMPLATE_DETECT_CHANGES_THROTTLE_MS)).subscribe(() => {
-      this._embeddedViewRef.detectChanges();
-    });
+    this._ngZoneSubscription = ngZone.onStable
+      .pipe(throttleTime(TEMPLATE_DETECT_CHANGES_THROTTLE_MS, undefined, { leading: true, trailing: true }))
+      .subscribe(() => {
+        this._embeddedViewRef.detectChanges();
+      });
   }
 
   componentWillUnmount() {
