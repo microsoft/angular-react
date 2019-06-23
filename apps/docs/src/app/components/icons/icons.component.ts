57b52e65108c51200bc4fc1d481d76dd2ba97572
Shai Rose <shai.av.rose@gmail.com>
2019-06-23 11:09:53 +0300

    upgrade (angular 8): removed unused files

--- apps/docs/src/app/components/icons/icons.component.ts
+++ apps/docs/src/app/components/icons/icons.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
-  styleUrls: ['./icons.component.scss'],
 })
 export class IconsComponent {}
--- apps/docs/src/app/containers/docs/wrappers/wrappers.component.ts
+++ apps/docs/src/app/containers/docs/wrappers/wrappers.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-wrappers',
   templateUrl: './wrappers.component.html',
-  styleUrls: ['./wrappers.component.scss'],
 })
 export class WrappersComponent implements OnInit {
   constructor() {}
