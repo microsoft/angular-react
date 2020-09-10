919f8a960a1cecbb20dacdc137463b61a8959193
Yan Qiu <qiuya@microsoft.com>
2020-09-10 14:29:24 +0800

    chore: change projec structor of fabric lib

--- libs/fabric/public-api.ts
+++ libs/fabric/public-api.ts
@@ -1,4 +1,45 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-export * from './src/public-api';
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export * from './lib/components/breadcrumb/public-api';
+export * from './lib/components/button/public-api';
+export * from './lib/components/calendar/public-api';
+export * from './lib/components/callout/public-api';
+export * from './lib/components/checkbox/public-api';
+export * from './lib/components/choice-group/public-api';
+export * from './lib/components/combo-box/public-api';
+export * from './lib/components/command-bar/public-api';
+export * from './lib/components/contextual-menu/public-api';
+export * from './lib/components/date-picker/public-api';
+export * from './lib/components/details-list/public-api';
+export * from './lib/components/dialog/public-api';
+export * from './lib/components/divider/public-api';
+export * from './lib/components/dropdown/public-api';
+export * from './lib/components/fabric/public-api';
+export * from './lib/components/group/public-api';
+export * from './lib/components/grouped-list/public-api';
+export * from './lib/components/hover-card/public-api';
+export * from './lib/components/icon/public-api';
+export * from './lib/components/image/public-api';
+export * from './lib/components/link/public-api';
+export * from './lib/components/marquee-selection/public-api';
+export * from './lib/components/message-bar/public-api';
+export * from './lib/components/modal/public-api';
+export * from './lib/components/panel/public-api';
+export * from './lib/components/persona/public-api';
+export * from './lib/components/pivot/public-api';
+export * from './lib/components/search-box/public-api';
+export * from './lib/components/shimmer/public-api';
+export * from './lib/components/slider/public-api';
+export * from './lib/components/spin-button/public-api';
+export * from './lib/components/spinner/public-api';
+export * from './lib/components/text-field/public-api';
+export * from './lib/components/toggle/public-api';
+export * from './lib/components/tooltip/public-api';
+export * from './lib/components/nav/public-api';
+export * from './lib/components/pickers/public-api';
+export * from './lib/components/progress-indicator/public-api';
+
